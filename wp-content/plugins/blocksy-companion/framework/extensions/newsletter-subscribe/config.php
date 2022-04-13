1e39ceefec5c74975f21f2491181c95e4949bf69
Ammar Rehan <admin@ammarrehan.live>
2022-04-13 19:37:09 -0400

    repo cleaned

--- wp-content/plugins/blocksy-companion/framework/extensions/newsletter-subscribe/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = [
-	'name' => __('Newsletter Subscribe', 'blocksy-companion'),
-	'description' => __('Easily capture new leads for your newsletter with the help of a widget, shortcode or even a block inserted on your pages or posts.', 'blocksy-companion')
-];
-
