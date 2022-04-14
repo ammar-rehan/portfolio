e55a827c87e66ae1a487716bc6a2eb385fea6436
Ammar Rehan <admin@ammarrehan.live>
2022-04-13 21:10:29 -0400

    test4

--- wp-content/plugins/blocksy-companion/framework/extensions/cookies-consent/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config = [
-	'name' => __('Cookies Consent', 'blocksy-companion'),
-	'description' => __('Display a cookie acceptance box in order to comply with the privacy regulations in your country.', 'blocksy-companion')
-];
--- wp-content/plugins/blocksy-companion/framework/extensions/trending/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config = [
-	'name' => __('Trending Posts', 'blocksy-companion'),
-	'description' => __('Highlight your most popular posts or products based on the number of comments or reviews they have gotten in the specified period of time.', 'blocksy-companion')
-];
